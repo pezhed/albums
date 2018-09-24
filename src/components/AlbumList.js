f8f38868ed5ac2f1f6dc4a41915f9ee21d77015a
Eric Lopez <ericlopez07@gmail.com>
2018-09-24 06:24:00 -0600

    Setup Album list to fetch data using http request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+//class componennt used to fetch data
+class AlbumList extends Component {
+  state = { albums: [] };
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-};
+  componentWillMount(){
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => response.json())
+    .then(responseData =>
+      this.setState({ albums: responseData }));
+  }
+
+
+  render() {
+    console.log(this.state);
+    return (
+      <View>
+        <Text>Album List!!!</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
