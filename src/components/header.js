49894046fc2fa8cec3c7a05a2a0f0934361201fa
Eric Lopez <ericlopez07@gmail.com>
2018-09-25 06:38:50 -0600

    Added images and button press events

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,9 @@ const Header = (props) => {
 
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>{props.headerText}</Text>
+      <Text style={textStyle}>
+        {props.headerText}
+      </Text>
     </View>
   );
 };
